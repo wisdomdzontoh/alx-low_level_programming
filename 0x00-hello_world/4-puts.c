6879c9224a3c544018f1bedfadcadc8c93f6229c
wisdomdzontoh <Wisdom Dzontoh>
2022-06-30 20:41:03 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/
- * main - Entry point
- * Return: Always 0 (sucess)
- */
+/**
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
