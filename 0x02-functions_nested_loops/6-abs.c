edb1ab38142ade436ad85fcbd4637502653744b5
wisdomdzontoh <Wisdom Dzontoh>
2022-07-05 14:49:18 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of int
+ *
+ * @a: as int
+ *
+ * Return: abs value
+ */
+int _abs(int a)
+{
+
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+}
