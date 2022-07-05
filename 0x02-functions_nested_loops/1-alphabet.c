de6492df79b2c0a969f3a2da004b9b0946da5947
wisdomdzontoh <Wisdom Dzontoh>
2022-07-05 14:14:27 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a-z
+ *
+ * Return: Always void
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
