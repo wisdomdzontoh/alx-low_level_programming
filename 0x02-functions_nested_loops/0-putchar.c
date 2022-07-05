f238a1730a18d4038fbe39b3571cc4e253ab5198
wisdomdzontoh <Wisdom Dzontoh>
2022-07-05 13:55:39 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include 'main.h'
-/*
- * main - print _putchar
- * Returns: always 0
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char joy[9] = "_putchar";
+	int i;
 
+	for (i = 0; i < 8; i = i + 1)
+	{
+		_putchar(joy[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
