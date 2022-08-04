b68d1d720699f585b0b651818e25544730479526
wisdomdzontoh <Wisdom Dzontoh>
2022-08-04 06:54:33 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
 * main -  writes program name
 * @argc: arguement count
 * @argv: array of pointers to CLI arguement
 * Return: 0
-**/
+*/
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 
